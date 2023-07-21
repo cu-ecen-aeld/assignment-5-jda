c4c564f3e6786684bd0731d213e7552e133aef7c
Jade Angrboða <jade@trashwitch.dev>
2023-07-20 23:19:41 -0600

    assignment 7 mods

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-LDD_VERSION = 627db11
+LDD_VERSION = 0676c10
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -18,22 +18,7 @@ LDD_MODULE_SUBDIRS += misc-modules
 
 LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
-# define LDD_BUILD_CMDS
-# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/mist-modules modules
-# 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull modules
-# endef
-
-# define LDD_INSTALL_TARGET_CMDS
-# 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-# 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-# 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-# 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
-# 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-# 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
-
-# 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
-# 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-# endef
+BR2_ROOTFS_OVERLAY += ../base_external/rootfs_overlay
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
